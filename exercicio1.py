58e5b65782a8f51c87754a1de03b4522725c1df8
Gilberto Pavuna <gilbertopavunacosta@hotmail.com>
2022-12-14 10:12:33 -0300

    Exercicio 1 resolvido

---
+++ exercicio1.py
@@ -0,0 +1,6 @@
+numero1 = int(input('Entre com o primeiro número: '))
+numero2 = int(input('Entre com o segundo número: '))
+if numero1 > numero2:
+    print(f'O maior numero é: {numero1}')
+else:
+    print(f'O maior numero é: {numero2}')
