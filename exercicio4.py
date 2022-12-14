119a6a91a36f83432cd3d055baa9cf1906971e61
Gilberto Pavuna <gilbertopavunacosta@hotmail.com>
2022-12-14 10:34:12 -0300

    Exercicio 4 resolvido

---
+++ exercicio4.py
@@ -0,0 +1,5 @@
+letra = input('Entre com uma letra do alfabeto: ').upper()
+if letra == 'a' or letra == 'e' or letra == 'i' or letra == 'o' or letra == 'u':
+    print(f'A letra {letra} é uma volgal.')
+else:
+    print(f'A letra {letra} é uma consoante.')
