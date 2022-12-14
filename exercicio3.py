e83fc0f2dfa9a3d6ae507ba20ccfbd9f7938b1fc
Gilberto Pavuna <gilbertopavunacosta@hotmail.com>
2022-12-14 10:26:59 -0300

    Exercicio 3 resolvido

---
+++ exercicio3.py
@@ -0,0 +1,7 @@
+genero = input('Entre com M para masculino ou F para feminino: ').upper()
+if genero == 'M':
+    print('Gênero Masculino')
+elif genero == 'F':
+    print('Gênero Feminino')
+else:
+    print('Gênero inválido')
