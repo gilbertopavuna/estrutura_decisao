0b0a61052892f0f01951e8cf4028232c4e7e4afd
Gilberto Pavuna <gilbertopavunacosta@hotmail.com>
2022-12-15 16:25:11 -0300

    altera dados

---
+++ exercicio9.py
@@ -0,0 +1,5 @@
+numero_1 = float(input('Entre com o primeiro número: '))
+numero_2 = float(input('Entre com o segundo número: '))
+numero_3 = float(input('Entre com o terceiro número: '))
+numeros = numero_1, numero_2, numero_3
+print(sorted(numeros))
