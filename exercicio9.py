f531de31076ed71f8e71e3b485b5b957e320f48d
Gilberto Pavuna <gilbertopavunacosta@hotmail.com>
2022-12-15 16:32:46 -0300

    atualiza usando o reverse

--- exercicio9.py
+++ exercicio9.py
@@ -1,5 +1,6 @@
 numero_1 = float(input('Entre com o primeiro número: '))
 numero_2 = float(input('Entre com o segundo número: '))
 numero_3 = float(input('Entre com o terceiro número: '))
-numeros = numero_1, numero_2, numero_3
-print(sorted(numeros))
+numeros = [numero_1, numero_2, numero_3]
+numeros.reverse()
+print(numeros)
